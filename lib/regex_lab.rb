af45853d8868b129bf8c469c0470e321a4c1ecf4
Joshua Crum <joshua.crum@gmail.com>
2019-05-13 19:59:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
   if word.match(/^[aeiouAEIOU]+\w/)
-    true
+    return true
   else
-    false
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
