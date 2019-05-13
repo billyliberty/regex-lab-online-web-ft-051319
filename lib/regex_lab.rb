8561a18555b0e74fce797a6429a75b7225501b7f
Joshua Crum <joshua.crum@gmail.com>
2019-05-13 20:15:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if match.phone(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/)
+  if match.phone(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$\b/)
     true
   else
     false
